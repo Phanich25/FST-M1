289273d90e03c30da80a37db6d6452a959601681
Phanich25 <76997439+Phanich25@users.noreply.github.com>
2022-05-03 12:26:48 -0700

    Add files via upload
    Activity 8 -- Custom Exeption.

---
+++ Java/Activities/CustomException.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class CustomException  extends Exception{
+    private String message=null;
+    public CustomException(String m)
+    {
+        this.message=m;
+    }
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+}
