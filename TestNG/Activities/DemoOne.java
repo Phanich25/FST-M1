b1660c59a4dc424e3a64d5151c83f24116bdd761
Phanich25 <76997439+Phanich25@users.noreply.github.com>
2022-05-10 07:10:53 -0700

    Add files via upload

---
+++ TestNG/Activities/DemoOne.java
@@ -0,0 +1,16 @@
+package suiteExample;
+
+import org.testng.annotations.Test;
+
+public class DemoOne {
+    @Test
+    public void firstTestCase()
+    {
+        System.out.println("Executing first test case from the DemoOne Class.");
+    }
+    @Test
+    public void secondTestCase()
+    {
+        System.out.println("Executing second test case from DemoOne class.");
+    }
+}
